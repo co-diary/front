718f1949fbb9aeb4f768e5b9687c190639100e0f
hb_kim <binikim66@gmail.com>
2023-02-08 15:57:01 +0900

    Feat:  버튼 컴포넌트 구현 #3

---
+++ my-app/src/components/common/Button.jsx
@@ -0,0 +1,107 @@
+import styled, { css } from 'styled-components';
+import Theme from '../../styles/Theme';
+// import Theme from '../../styles/Theme';
+
+// const SButton = styled.button`
+//   background-color: ${Theme.MAIN};
+//   padding: 1rem 2.8rem;
+//   border-radius: 3rem;
+// `;
+
+const setSize = (size) => {
+  switch (size) {
+    case 'lg':
+      return css`
+        width: 35rem;
+        padding: 1.4rem 0;
+        border-radius: 4.4rem;
+
+        &:active,
+        &:hover {
+          color: ${Theme.MAIN_GRAY};
+          background-color: #fcd240;
+        }
+
+        &:disabled {
+          background-color: #ffe58e;
+        }
+      `;
+    case 'md':
+      return css`
+        width: 12rem;
+        padding: 1rem 0;
+        border-radius: 3rem;
+
+        &:active,
+        &:hover {
+          color: ${Theme.MAIN_GRAY};
+          background-color: #fcd240;
+        }
+
+        &:disabled {
+          background-color: #ffe58e;
+        }
+      `;
+    case 'ms':
+      return css`
+        width: 9.5rem;
+        padding: 0.8rem 0;
+        border-radius: 3rem;
+
+        &:active,
+        &:hover {
+          color: ${Theme.MAIN_GRAY};
+          background-color: #fcd240;
+        }
+
+        &:disabled {
+          background-color: #ffe58e;
+        }
+      `;
+    case 'sm':
+      return css`
+        width: 5.6rem;
+        padding: 0.8rem 0;
+        border-radius: 3rem;
+
+        &:active,
+        &:hover {
+          color: ${Theme.MAIN_GRAY};
+          background-color: #fcd240;
+        }
+
+        &:disabled {
+          background-color: #ffe58e;
+        }
+      `;
+    default:
+      return css`
+        width: 9.5rem;
+        background-color: ${Theme.WHITE};
+        padding: 0.8rem 0;
+        border: 1px solid ${Theme.BORDER};
+        border-radius: 3rem;
+        font-size: 1.4rem;
+        font-family: LINESeedKR-bd;
+
+        &:active,
+        &:hover {
+          color: ${Theme.MAIN_GRAY};
+          background-color: ${Theme.BORDER};
+        }
+      `;
+  }
+};
+
+const SButton = styled.button`
+  font-size: 1.4rem;
+  font-family: LINESeedKR-bd;
+  background-color: ${Theme.MAIN};
+  ${({ size }) => setSize(size)};
+`;
+
+function Button({ text, size, onClick }) {
+  return <SButton size={size} onClick={onClick}>{text}</SButton>;
+}
+
+export default Button;
--- my-app/src/pages/Home/Home.jsx
+++ my-app/src/pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Button from '../../components/common/Button';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Button size='lg' text='회원가입' />
+      <Button size='md' text='회원가입(md)'/>
+      <Button size='ms' text='회원가입(ms)' />
+      <Button size='sm' text='등록'/>
+      <Button size='profile' text='프로필수정'/>
+    </>
+  );
 }
 
 export default Home;
