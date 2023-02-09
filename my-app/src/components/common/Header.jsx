fc7db0967fbb4e91da08491b110c5d5dea97bc44
Kimseolha <kynwww@naver.com>
2023-02-09 18:04:54 +0900

    Design: 헤더 기본 스타일링 #8 아이콘 적용, 좌/우 메뉴 나눠 구현

--- my-app/src/components/common/Header.jsx
+++ my-app/src/components/common/Header.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import Theme from '../../styles/Theme';
 import IconBack from '../../assets/Icon-Back.png';
+import IconHeartOff from '../../assets/Icon-Heart-off.png';
+import IconMore from '../../assets/Icon-More.png';
 
-const HeaderContainer = styled.header`
+const Container = styled.header`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
+  right: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height: 4.8rem;
-  background-color: pink;
+  background-color: ${Theme.WHITE};
   padding: 0 2rem;
   z-index: 99;
   border-bottom: 1px solid ${Theme.BORDER};
@@ -17,15 +22,31 @@ const HeaderContainer = styled.header`
   img {
     width: 2.4rem;
     height: 2.4rem;
-    background-color: red;
+    margin-right: 0.6rem;
   }
+`;
+
+const Menu = styled.div`
+  display: flex;
+`;
+
+const PageTitle = styled.div`
+  font-size: 1.8rem;
+  padding-top: 0.4rem;
 `;
 
 function Header() {
   return (
-    <HeaderContainer>
-      <img src={IconBack} alt='뒤로가기' />
-    </HeaderContainer>
+    <Container>
+      <Menu>
+        <img src={IconBack} alt='뒤로가기' />
+        <PageTitle>음료</PageTitle>
+      </Menu>
+      <Menu>
+        <img src={IconHeartOff} alt='' />
+        <img src={IconMore} alt='' />
+      </Menu>
+    </Container>
   );
 }
 
