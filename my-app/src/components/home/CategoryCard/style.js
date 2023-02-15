5d030a865b241fcc79d1f401d822dba0de3cf7d2
Kimseolha <kynwww@naver.com>
2023-02-16 02:25:02 +0900

    Feat: CategoryCard 컴포넌트 생성 #20

---
+++ my-app/src/components/home/CategoryCard/style.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import Theme from '../../../styles/Theme';
+
+export const Container = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 16.4rem;
+  height: 16.4rem;
+  border-radius: 1rem;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.14);
+  font-family: 'LINESeedKR-Bd';
+  background-color: ${Theme.WHITE};
+`;
+
+export const Count = styled.p`
+  position: absolute;
+  text-align: right;
+  width: 5rem;
+  top: 1.2rem;
+  right: 1.6rem;
+  font-size: 1.4rem;
+  color: ${Theme.MAIN_GRAY};
+`;
+
+export const ImgContainer = styled.figure`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CategoryImg = styled.img`
+  width: 8.6rem;
+  height: 8.6rem;
+
+  object-fit: cover;
+`;
+
+export const CategoryTitle = styled.figcaption`
+  line-height: 2.118rem;
+  font-size: 1.6rem;
+`;
