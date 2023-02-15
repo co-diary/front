f2307ef3d1eafea40dec4b776cc5ab74135bb84b
Kimseolha <kynwww@naver.com>
2023-02-15 23:01:16 +0900

    Design: 메인 페이지 마크업 / 레이아웃 #20

--- my-app/src/pages/Home/Home.jsx
+++ my-app/src/pages/Home/Home.jsx
@@ -1,27 +1,63 @@
 import React from 'react';
-import styled from 'styled-components';
-import Button from '../../components/common/Button';
+import * as S from './style';
 import Header from '../../components/common/Header';
 import NavBar from '../../components/common/NavBar';
-
-const Wrapper = styled.main`
-  height: calc(100vh - 10.8rem);
-  display: flex;
-  flex-direction: column;
-  height: fit-content;
-`;
+import IconTag from '../../assets/Icon-tag.png';
+// import IconTagActive from '../../assets/Icon-tag-hover.png';
+import IconSearch from '../../assets/Icon-Search.png';
 
 function Home() {
   return (
     <>
-      <Header />
-      <Wrapper>
-        <Button size='lg' text='회원가입' />
-        <Button size='md' text='회원가입(md)' />
-        <Button size='ms' text='회원가입(ms)' />
-        <Button size='sm' text='등록' />
-        <Button size='profile' text='프로필수정' />
-      </Wrapper>
+      <Header
+        rightChild={
+          <>
+            <S.HeaderBtn>
+              <img src={IconTag} alt='태그 검색 바로가기' />
+            </S.HeaderBtn>
+            <S.HeaderBtn>
+              <img src={IconSearch} alt='검색 바로가기' />
+            </S.HeaderBtn>
+          </>
+        }
+      />
+      <S.Container>
+        <S.Section>
+          <S.SectionContainer>
+            <S.Title>오늘도 나만의 커디어리를 기록해 보세요!</S.Title>
+            <S.Total>
+              <S.TotalTxt>전체 기록</S.TotalTxt>
+              <S.Count>502</S.Count>
+            </S.Total>
+          </S.SectionContainer>
+        </S.Section>
+        <S.Section>
+          <S.SectionTitle>곽두팔이님의 기록 앨범</S.SectionTitle>
+          <S.Category>
+            <S.CategoryCount>102</S.CategoryCount>
+            <S.CategoryCard>
+              <img src='' alt='' />
+              <figcaption>음료</figcaption>
+            </S.CategoryCard>
+          </S.Category>
+          <S.Category>
+            <p>300+</p>
+            <S.CategoryCard>
+              <img src='' alt='' />
+              <figcaption>디저트</figcaption>
+            </S.CategoryCard>
+          </S.Category>
+        </S.Section>
+        <S.Section>
+          <S.SectionTitle>최근 추가된 기록</S.SectionTitle>
+          <S.Cards>
+            <S.Card />
+            <S.Card />
+            <S.Card />
+            <S.Card />
+          </S.Cards>
+        </S.Section>
+      </S.Container>
       <NavBar />
     </>
   );
---
+++ my-app/src/pages/Home/style.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+import Theme from '../../styles/Theme';
+
+export const Container = styled.main`
+  height: calc(100vh - 10.8rem);
+  padding-top: calc(4.8rem + 2.6rem);
+  display: flex;
+  flex-direction: column;
+  height: fit-content;
+`;
+
+export const HeaderBtn = styled.button`
+  background-color: ${Theme.WHITE};
+  img {
+    width: 2.4rem;
+    height: 2.4rem;
+    object-fit: cover;
+  }
+`;
+
+export const Section = styled.section`
+  & + section {
+    margin-top: 2.6rem;
+  }
+`;
+
+export const SectionTitle = styled.h3`
+  font-family: 'LINESeedKR-Bd';
+  font-size: 1.4rem;
+  margin-bottom: 1.6rem;
+`;
+
+export const SectionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h2`
+  width: 18.3rem;
+  font-family: 'LINESeedKR-Bd';
+  font-size: 1.8rem;
+`;
+
+export const Total = styled.div`
+  position: relative;
+  font-family: 'LINESeedKR-Bd';
+`;
+
+export const TotalTxt = styled.p`
+  display: inline-block;
+  padding: 0.4rem 1rem;
+  height: 2.4rem;
+  background-color: ${Theme.MAIN};
+  border-radius: 3rem;
+  line-height: 1.589rem;
+  font-size: 1.2rem;
+`;
+
+export const Count = styled.p`
+  position: absolute;
+  top: calc(2.4rem + 0.8rem);
+  right: 0.8rem;
+  font-size: 1.6rem;
+`;
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2.2rem;
+`;
+
+export const Category = styled.div`
+  display: inline-block;
+  width: 16.4rem;
+  height: 16.4rem;
+  border: 1px solid black;
+  border-radius: 1rem;
+`;
+
+export const CategoryCount = styled.p``;
+
+export const CategoryCard = styled.figure``;
+
+export const Cards = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+`;
+
+export const Card = styled.div`
+  width: 100%;
+  height: 18.4rem;
+  border: 1px solid black;
+  border-radius: 1rem;
+`;
