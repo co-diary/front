f2c1e623edf1f7cbc6a203d7ec102e76288be201
hb_kim <binikim66@gmail.com>
2023-02-16 23:12:10 +0900

    Design: 게시글 리스트 페이지 마크업과 스타일링 #25

--- my-app/src/routes/Router.jsx
+++ my-app/src/routes/Router.jsx
@@ -9,7 +9,7 @@ import Location from '../pages/Location';
 import MyPage from '../pages/MyPage';
 import ProfileEdit from '../pages/MyPage/ProfileEdit';
 import NotFound from '../pages/NotFound/NotFound';
-import Post from '../pages/Post/Post';
+import Post from '../pages/Post';
 import PostDetail from '../pages/Post/PostDetail';
 import PostEdit from '../pages/Post/PostEdit/PostEdit';
 import PostUpload from '../pages/Post/PostUpload/PostUpload';
