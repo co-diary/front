08dc629cb05d37b344e110813ff6b325b0117d4c
wSeungMi <seungmi.dev@gmail.com>
2023-02-17 00:06:22 +0900

    Design: 좋아요 페이지 마크업 #37

--- my-app/src/pages/LikePosts/index.jsx
+++ my-app/src/pages/LikePosts/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/common/Header';
 import NavBar from '../../components/common/NavBar';
+import * as S from './style';
 
 function LikePosts() {
   return (
     <>
       <Header title='좋아요' />
+      <S.Container>카드 컴포넌트</S.Container>
       <NavBar page='liked' />
     </>
   );
