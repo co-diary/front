eb217220c7d8a4d9801e8f3d0e73f3bf19559660
wSeungMi <seungmi.dev@gmail.com>
2023-02-16 22:25:06 +0900

    Design: 태그 앵커 마크업 및 여백 수정 태그 모아보기 페이지

--- my-app/src/pages/Hashtag/style.js
+++ my-app/src/pages/Hashtag/style.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Theme from '../../styles/Theme';
 
@@ -5,20 +6,25 @@ export const Container = styled.div`
   padding: 4.8rem 0 6rem;
 `;
 
-export const TagList = styled.ul`
+export const TagBox = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  gap: 1.2rem;
+  gap: 1.6rem 1.2rem;
   width: 100%;
   padding: 2.6rem 0;
 `;
 
-export const Tag = styled.li`
+export const TagList = styled.li`
+  padding: 0.5rem 0;
+`;
+
+export const TagLink = styled(Link)`
   padding: 0.5rem 1rem;
-  height: 2.7rem;
+  border-radius: 3rem;
   font-size: 1.4rem;
   line-height: 1.8rem;
   color: ${Theme.WHITE};
   border-radius: 3rem;
   background-color: ${Theme.SUB_ORANGE};
+  text-decoration: none;
 `;
