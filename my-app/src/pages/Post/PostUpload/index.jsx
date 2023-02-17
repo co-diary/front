0d6ccedaf71a6dfaa7afa265d1b056926c9b1778
wSeungMi <seungmi.dev@gmail.com>
2023-02-17 12:40:04 +0900

    Design: 헤더, 네비 공통 컴포넌트 적용 #39

--- my-app/src/pages/Post/PostUpload/index.jsx
+++ my-app/src/pages/Post/PostUpload/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from '../../../components/common/Header';
+import NavBar from '../../../components/common/NavBar';
+import Button from '../../../components/common/Button';
 
 function PostUpload() {
-  return <div>PostUpload</div>;
+  return (
+    <>
+      <Header title='오늘 작성할 커디어리' rightChild={<Button size='sm' text='등록' />} />
+      <NavBar page='upload' />
+    </>
+  );
 }
 
 export default PostUpload;
