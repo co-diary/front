b4072ac5f75663ce2e4c8cf7711803b6b25c7dec
Kimseolha <kynwww@naver.com>
2023-02-19 21:35:16 +0900

    Design: 검색 페이지 스타일링 #28

---
+++ my-app/src/pages/Search/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchHeader from '../../components/common/SearchHeader';
+import * as S from './style';
+
+function Search() {
+  return (
+    <>
+      <SearchHeader />
+      <S.Container>
+        <S.logo>로고</S.logo>
+      </S.Container>
+    </>
+  );
+}
+
+export default Search;
