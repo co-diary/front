181f066911cb24512933e2d5a03b040793efaa5b
jhfrontend <kwonjhjyj@naver.com>
2023-02-19 21:25:24 +0900

    Style: 지도페이지 eslint 오류 수정 #43

--- my-app/src/pages/Location/index.jsx
+++ my-app/src/pages/Location/index.jsx
@@ -4,15 +4,17 @@ import Header from '../../components/common/Header';
 import NavBar from '../../components/common/NavBar';
 
 function Location () {
-	const [location, setLocation] = useState(null); // 현재 위치를 저장할 상태
-  const [position, setPosition] = useState();
+	// 현재 위치를 저장할 상태
+	const [location, setLocation] = useState(null); 
+	const [position, setPosition] = useState();
 
 	useEffect(() => {
-    
-		navigator.geolocation.getCurrentPosition(isSuccess, isError); // 성공시 isSuccess, 실패시 isError 함수가 실행된다.
+
+		// 성공시 isSuccess, 실패시 isError 함수가 실행됩니다.
+		navigator.geolocation.getCurrentPosition(isSuccess, isError);
 	}, []);
 
-  // 성공하면 사용자가 현재 위치한 위도와 경도를 나타냅니다.
+	// 성공하면 사용자가 현재 위치한 위도와 경도를 나타냅니다.
 	const isSuccess = (response) => {
     const { latitude, longitude} = response.coords;
     
@@ -24,27 +26,27 @@ function Location () {
 	};
 
 	return (
-		<>
-      <Header title="지도" />
-			{location && (
-				<Map 
-                center={{ lat: location.latitude, lng: location.longitude }} 
-                style={{ width: '100%', height: '100vh' }} 
-                level={1}
-                onClick={(_t, MouseEvent) =>setPosition({
-                  lat: MouseEvent.latLng.getLat(),
-                  lng: MouseEvent.latLng.getLng(),
-                })}
-                >
-                  {position && <MapMarker position={position} />}
-					<MapMarker 
-            position={{ lat: location.latitude, lng: location.longitude }} 
-            draggable={true} // 마커가 드래그 가능하게 합니다.
-          />
-				</Map>
-			)}
-      <NavBar page = "location" />
-		</>
+	<>
+    <Header title="지도" />
+		{location && (
+		<Map 
+            center = {{ lat: location.latitude, lng: location.longitude }} 
+            style = {{ width: '100%', height: '100vh' }} 
+            level = {1}
+            onClick = {(_t, MouseEvent) =>setPosition({
+                lat: MouseEvent.latLng.getLat(),
+                lng: MouseEvent.latLng.getLng(),
+            })}
+        >
+            {position && <MapMarker position={position} />}
+		<MapMarker 
+			position = {{ lat: location.latitude, lng: location.longitude }} 
+            draggable = {true} // 마커가 드래그 가능하게 합니다.
+        />
+		</Map>
+		)}
+    <NavBar page = "location" />
+	</>
 	);
 }
 
