990d353d266558e470f399ac7d7f424a56a5cb43
hb_kim <binikim66@gmail.com>
2023-02-20 14:44:38 +0900

    Feat: 파이어스토어 설정 및 연동 #46

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './routes/Router';
+import firestore from './firebase';
 
 function App() {
+  useEffect(() => {
+    console.log(firestore);
+    const users = firestore.collection('users');
+
+    users
+      .doc('jSwWPHGDKXx2yXINq7K8')
+      .get()
+      .then((doc) => {
+        console.log(doc.data());
+        console.log(doc.id);
+      });
+  });
+
   return (
     <div className='App'>
       <Router />
---
+++ my-app/src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// firebaseConfig 정보로 firebase 시작
+firebase.initializeApp(firebaseConfig);
+
+// firebase의 firestore 인스턴스를 변수에 저장
+const firestore = firebase.firestore();
+
+// 필요한 곳에서 사용할 수 있도록 내보내기
+export default firestore;
