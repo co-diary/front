d827a62b782972ddd6334e4b9fa804166ec9aefd
jhfrontend <kwonjhjyj@naver.com>
2023-02-24 10:47:44 +0900

    Feat: 마이페이지 기능 구현 #53

--- my-app/src/pages/MyPage/index.jsx
+++ my-app/src/pages/MyPage/index.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
 import * as S from './style';
 import Header from '../../components/common/Header';
 import NavBar from '../../components/common/NavBar';
 import Button from '../../components/common/Button';
-
-
+import ConfirmModal from '../../components/modal/ConfirmModal';
+import Portal from '../../components/modal/Potal';
+import useToggle from '../../hooks/useToggle';
 
 
 function MyPage() {
+  const [isModalOpen, setIsModalOpen] = useToggle();
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const currentUser = auth.currentUser;
+
+    if (currentUser) {
+      setUser({
+        displayName: currentUser.displayName,
+        email: currentUser.email,
+      });
+    }
+  }, [auth]);
+
+
+  const onLogOutClick = () => {
+    signOut(auth);
+    navigate('/login');
+  };
+  const onCancelClick = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
     <Header title="마이 페이지"/>
     <S.Container>
       <S.BoxOne>
         <div>
-          <S.UserName>곽두팔이</S.UserName>
-          <S.UserEmail>email@abc.com</S.UserEmail>
+          <S.UserName>{user?.displayName || '사용자 이름'}</S.UserName>
+          <S.UserEmail>{user?.email || 'email@abc.com'}</S.UserEmail>
         </div>
         <S.ButtonContainer>
           <Button size='default' text='프로필 수정'>
@@ -24,22 +51,38 @@ function MyPage() {
           </Button>
         </S.ButtonContainer>
       </S.BoxOne>
-      <div>
-        <ul>
-          <S.MyPageList>
-          <S.WebVersionContainer>
-            <p>공지사항</p>
-            <S.WebVersion>v.1.0.0</S.WebVersion>
-          </S.WebVersionContainer>
-          </S.MyPageList>
-          <S.MyPageList>문의하기</S.MyPageList>
-          <S.MyPageList>고객센터/운영정책</S.MyPageList>
-          <S.MyPageList>로그아웃</S.MyPageList>
-          <S.Deactivate>탈퇴하기</S.Deactivate>
-        </ul>
-      </div>
-      <NavBar page="myPage"/> 
+      <S.BoxTwo>
+        <S.NoticeContainer>
+          <div>
+            <button>공지사항</button>
+          </div>
+          <S.WebVersion>v.1.0.0</S.WebVersion>
+        </S.NoticeContainer>
+        <S.MyPageLists>
+          <button>문의하기</button>
+        </S.MyPageLists>
+        <S.MyPageLists>
+          <button>고객센터/운영정책</button>
+          </S.MyPageLists>
+        <S.MyPageLists>
+          <button onClick={setIsModalOpen}>로그아웃</button>
+        </S.MyPageLists>
+        <S.Deactivate>탈퇴하기</S.Deactivate>
+      </S.BoxTwo>
     </S.Container>
+    <NavBar page="myPage"/>
+    <Portal>
+      {isModalOpen ? 
+        <ConfirmModal 
+          onClickClose={setIsModalOpen} 
+          visible={isModalOpen} 
+          msg='정말 로그아웃 하시겠습니까?' 
+          leftBtnMsg='확인'
+          leftOnclick={onLogOutClick} 
+          rightBtnMsg='취소'
+          rightOnclick={onCancelClick} 
+        /> : null}
+    </Portal> 
     </>
   )
 }
