41af59157e12e55f3f7e59898c1cdf82580e439f
Kimseolha <kynwww@naver.com>
2023-02-28 15:54:09 +0900

    Style: Home.jsx를 index.jsx로 컨벤션 맞게 수정

--- my-app/src/routes/Router.jsx
+++ my-app/src/routes/Router.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Hashtag from '../pages/Hashtag';
 import HashtagResult from '../pages/Hashtag/HashtagResult';
-import Home from '../pages/Home/Home';
+import Home from '../pages/Home';
 import LikePosts from '../pages/LikePosts/';
 import Login from '../pages/Login';
 import Location from '../pages/Location';
