1073117e813e6bb21dc9056af011e7119c8ddd54
Kimseolha <kynwww@naver.com>
2023-03-20 18:26:19 +0900

    Refactor: getPost 함수 내부 중복 코드 제거 #74

--- my-app/src/hooks/getPost.js
+++ my-app/src/hooks/getPost.js
@@ -1,10 +1,9 @@
 import { getDocs, query, collection, where } from 'firebase/firestore';
 import { db } from '../firebase';
 
-// 재사용 가능하도록 디벨롭 예정
-
 async function getPost(keyOption, target) {
   const postList = [];
+  const q = query(collection(db, 'post'));
 
   const getData = (postSnapshot) => {
     postSnapshot.forEach((doc) => {
@@ -18,20 +17,16 @@ async function getPost(keyOption, target) {
   };
 
   if (keyOption === 'ALL') {
-    const q = query(collection(db, 'post'));
     const postSnapshot = await getDocs(q);
 
     getData(postSnapshot);
-    console.log(postList);
-    return postList;
   } else {
-    const q = query(collection(db, 'post'), where(keyOption, '==', target));
-    const postSnapshot = await getDocs(q);
+    const postSnapshot = await getDocs(query(q, where(keyOption, '==', target)));
 
     getData(postSnapshot);
-    console.log(postList);
-    return postList;
   }
+  console.log(postList);
+  return postList;
 }
 
 export default getPost;
