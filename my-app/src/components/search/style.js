f71ca8bf4956b7f41dc71c2d9e50da545753b1cc
Kimseolha <kynwww@naver.com>
2023-03-24 15:58:11 +0900

    Style: Search style 코드 수정 - 이미지 비율 에러 해결, 취소 버튼 CSS #81

--- my-app/src/components/search/style.js
+++ my-app/src/components/search/style.js
@@ -14,16 +14,13 @@ export const Container = styled.header`
   padding: 0 2rem;
   z-index: 99;
   border-bottom: 1px solid ${Theme.BORDER};
-  gap: 1rem;
 `;
 
 export const Button = styled.button`
   background-color: ${Theme.WHITE};
-  width: 2.4rem;
-  height: 2.4rem;
   img {
-    width: 100%;
-    height: 100%;
+    width: 2.4rem;
+    height: 2.4rem;
     margin-right: 0.6rem;
   }
 `;
@@ -48,7 +45,7 @@ export const Input = styled.input`
   padding: 1rem 0 1rem 1.4rem;
   border: 1px solid ${Theme.BORDER};
   border-radius: 3.2rem;
-  margin-right: 0.6rem;
+
   transition: all linear 0.4s;
   &:focus {
     width: 90%;
@@ -61,6 +58,10 @@ export const Input = styled.input`
 `;
 
 export const CancelBtn = styled.button`
-  width: 3rem;
+  width: 5rem;
+  color: #3d3d3d;
+  padding: 1.3rem 0 1.3rem 0;
   border: 1px solid red;
+  font-size: 1.4rem;
+  font-family: 'LINESeedKR-Bd';
 `;
