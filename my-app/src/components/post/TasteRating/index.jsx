31f250588992d620365b7497e2e844ab25140f00
wSeungMi <seungmi.dev@gmail.com>
2023-04-07 23:13:36 +0900

    Feat: hover 관련 마우스 이벤트&함수 TasteRating 컴포넌트에 추가 #85

--- my-app/src/components/post/TasteRating/index.jsx
+++ my-app/src/components/post/TasteRating/index.jsx
@@ -6,7 +6,12 @@ const SRatingBox = styled.div`
   width: calc(100% - 5.6rem);
 `;
 
-function TasteRating({ ratingClicked, setRatingClicked }) {
+function TasteRating({
+  ratingClicked,
+  handleStarRatingClicked,
+  ratingHovered,
+  handleMouseChecked,
+}) {
   const ratingIndex = [0, 1, 2, 3, 4];
 
   return (
@@ -14,11 +19,13 @@ function TasteRating({ ratingClicked, setRatingClicked }) {
       {ratingIndex.map((rating, i) => (
         <Star
           key={i}
-          fill={ratingClicked > rating ? '#F7DA76' : '#BDBDBD'}
+          fill={(ratingClicked > rating) | (ratingHovered > rating) ? '#F7DA76' : '#BDBDBD'}
           width='2rem'
           height='2rem'
           cursor='pointer'
-          onClick={() => setRatingClicked(rating + 1)}
+          onMouseEnter={() => handleMouseChecked(rating + 1)}
+          onMouseLeave={() => handleMouseChecked(0)}
+          onClick={() => handleStarRatingClicked(rating + 1)}
         />
       ))}
     </SRatingBox>
