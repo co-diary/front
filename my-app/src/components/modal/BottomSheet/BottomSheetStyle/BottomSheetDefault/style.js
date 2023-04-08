bb3b099f5db155563883af2ee83a182dab8f144a
Kimseolha <kynwww@naver.com>
2023-04-08 21:06:15 +0900

    Feat: 기본 바텀시트 UX 성능 개선 - 항목 선택 범위 좌우 100% #95 & 코드 정리

--- my-app/src/components/modal/BottomSheet/BottomSheetStyle/BottomSheetDefault/style.js
+++ my-app/src/components/modal/BottomSheet/BottomSheetStyle/BottomSheetDefault/style.js
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
 import Theme from '../../../../../styles/Theme';
 
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  min-height: 6rem;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+  background-color: ${Theme.WHITE};
+  padding: 3rem 1.2rem 1rem 1.2rem;
+`;
+
 export const Contents = styled.button`
   background-color: ${Theme.WHITE};
+  padding: 1.3rem 2.7rem 1.4rem 2.7rem;
+  width: 100%;
   text-align: left;
-  padding: 1.3rem 0 1.4rem 2.7rem;
   font-size: 1.4rem;
 `;
 
@@ -12,7 +25,7 @@ export const CloseHandler = styled.button`
   width: 5rem;
   height: 0.4rem;
   position: absolute;
-  top: 40%;
+  top: 10%;
   left: 50%;
   transform: translateX(-50%);
   border-radius: 0.5rem;
@@ -25,16 +38,4 @@ export const CloseHandler = styled.button`
     height: 2.5rem;
     margin-top: -1rem;
   }
-`;
-
-export const Container = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  min-height: 3.6rem;
-  border-top-left-radius: 1rem;
-  border-top-right-radius: 1rem;
-  background-color: ${Theme.WHITE};
-  padding: 1.2rem 2rem;
 `;
