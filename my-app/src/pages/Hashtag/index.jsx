043959f5513947a672a9aadea4d1b783c75c726f
Kimseolha <kynwww@naver.com>
2023-04-13 20:47:33 +0900

    Feat: 유저가 작성한 태그 없을 경우 문구 출력 #99

--- my-app/src/pages/Hashtag/index.jsx
+++ my-app/src/pages/Hashtag/index.jsx
@@ -1,35 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Header from '../../components/common/Header';
 import NavBar from '../../components/common/NavBar';
 import * as S from './style';
 
-import getPost from '../../hooks/getPost';
+// import getPost from '../../hooks/getPost';
 
 function Hashtag() {
   const [tagArr, setTagArr] = useState([]);
 
+  console.log(setTagArr);
   console.log(tagArr);
 
-  useEffect(() => {
-    getPost('ALL').then((data) => {
-      const onlyTags = data.reduce((acc, cur) => [...acc, ...cur.tag], []);
+  // useEffect(() => {
+  //   getPost('ALL').then((data) => {
+  //     const onlyTags = data.reduce((acc, cur) => [...acc, ...cur.tag], []);
 
-      setTagArr(onlyTags);
-    });
-  }, []);
+  //     setTagArr(onlyTags);
+  //   });
+  // }, []);
 
   return (
     <>
       <Header title='태그 모아보기' />
       <S.Container>
-        <S.TagBox>
-          {tagArr.map((tag) => (
-            <S.TagList key={uuidv4()}>
-              <S.TagLink to='/hashtag/keyword'>#{tag}</S.TagLink>
-            </S.TagList>
-          ))}
-        </S.TagBox>
+        {tagArr.length ? (
+          <S.TagBox>
+            {tagArr.map((tag) => (
+              <S.TagList key={uuidv4()}>
+                <S.TagLink to='/hashtag/keyword'>#{tag}</S.TagLink>
+              </S.TagList>
+            ))}
+          </S.TagBox>
+        ) : (
+          <>
+            <S.TextContainer>
+              <S.Text>
+                아직 등록된 <S.TextDeco>#해시태그</S.TextDeco> 가 없어요.
+              </S.Text>
+              <S.Text>
+                <S.TextDeco>#해시태그</S.TextDeco> 를 등록해보세요!
+              </S.Text>
+            </S.TextContainer>
+          </>
+        )}
       </S.Container>
       <NavBar />
     </>
