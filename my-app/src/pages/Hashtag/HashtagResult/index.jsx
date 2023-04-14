db269c28602ebe52dd4d17160c51d262325e9433
Kimseolha <kynwww@naver.com>
2023-04-14 20:11:43 +0900

    WIP: 해시태그 검색 기능 파이어베이스 코드 구현중 #101

--- my-app/src/pages/Hashtag/HashtagResult/index.jsx
+++ my-app/src/pages/Hashtag/HashtagResult/index.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router';
 import Header from '../../../components/common/Header';
 import NavBar from '../../../components/common/NavBar';
+import { firestore } from '../../../firebase';
+// import filterPosts from '../../../hooks/filterPosts';
 import * as S from './style';
 
 function HashtagResult() {
+  // const [post, setPost] = useState([]);
+
   const location = useLocation();
 
   const keyword = location.state.data;
+
+  const searchInArray = async (collection, tags, value) => {
+    const querySnapshot = await firestore
+      .collection(collection)
+      .where(tags, 'array-contains', value)
+      .get();
+
+    const results = [];
+
+    querySnapshot.forEach((doc) => {
+      results.push({ id: doc.id, data: doc.data() });
+    });
+
+    return results;
+  };
+
+  useEffect(() => {
+    console.log(keyword);
+
+    searchInArray('post', 'tags', keyword).then((results) => {
+      console.log(results);
+
+      console.log('Matching documents:');
+      results.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data);
+      });
+    });
+  }, []);
 
   return (
     <>
