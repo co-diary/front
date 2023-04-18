4b8adfefc3649f7bf23c51ba94f55b81afe9d1b9
Kimseolha <kynwww@naver.com>
2023-04-18 16:19:07 +0900

    Feat: 이번년도가 아닌 게시글의 경우 년도까지 출력 기능 #112

--- my-app/src/components/post/PostCard/index.jsx
+++ my-app/src/components/post/PostCard/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { doc, updateDoc } from 'firebase/firestore';
 import { v4 as uuidv4 } from 'uuid';
 import * as S from './style';
@@ -11,10 +11,32 @@ import IconStarOff from '../../../assets/Icon-star-off.png';
 import useToggle from '../../../hooks/useToggle';
 
 function PostCard({ id, date, like, location, menu, photo, review, score, shop, tags }) {
-  const slicedDate = date.toDate().toISOString().slice(5, 10).replace('-', '.');
   const scoreIndexs = [0, 1, 2, 3, 4];
-
   const [liked, setLiked] = useToggle(like);
+  const [formattedDate, setFormattedDate] = useState();
+
+  const formatDate = (dateFormatted) => {
+    const now = new Date();
+    const nowYear = now.getFullYear();
+    const dateString = dateFormatted.toISOString();
+    const slicedDate = dateString.slice(5, 10).replace('-', '.');
+
+    if (dateFormatted.getFullYear() === nowYear) {
+      setFormattedDate(slicedDate);
+    } else {
+      const anotherYearDate = dateString.slice(2, 10).replaceAll('-', '.');
+
+      setFormattedDate(anotherYearDate);
+      console.log(formattedDate);
+    }
+  };
+
+  useEffect(() => {
+    const dateFormatted = date.toDate();
+
+    console.log('년도', dateFormatted.getFullYear());
+    formatDate(dateFormatted);
+  }, []);
 
   const updatePost = async (postId, newLiked) => {
     const postDoc = doc(db, 'post', postId);
@@ -31,7 +53,7 @@ function PostCard({ id, date, like, location, menu, photo, review, score, shop, 
   return (
     <S.PostCardBox>
       <S.PostCover>
-        <span>{slicedDate}</span>
+        <span>{formattedDate}</span>
         {photo && <img src={photo} alt='메뉴 썸네일 사진' />}
       </S.PostCover>
       <S.PostContent>
