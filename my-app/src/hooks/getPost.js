7c4711bb90591f742175e5d4ef2bf15e774bab40
Kimseolha <kynwww@naver.com>
2023-04-18 20:10:51 +0900

    Feat: getPost - userId 전달받아 uid 필드와 비교하여 유효성 검사 후 postList 반환하도록 로직 변경 #108

--- my-app/src/hooks/getPost.js
+++ my-app/src/hooks/getPost.js
@@ -1,7 +1,7 @@
 import { getDocs, query, collection, where, orderBy } from 'firebase/firestore';
 import { db } from '../firebase';
 
-async function getPost(queryOption, target, option) {
+async function getPost(userId, queryOption, target, option) {
   const postList = [];
   const q = query(collection(db, 'post'));
 
@@ -16,18 +16,27 @@ async function getPost(queryOption, target, option) {
     });
   };
 
+  // uid 값의 유효성 검사
+  if (!userId || typeof userId !== 'string') {
+    console.log(userId, '유효성 검사에 걸려버림');
+    return postList;
+  }
+
+  // where() 함수를 사용하여 uid 필드와 일치하는 문서를 검색
+  const queryWithUid = query(q, where('uid', '==', userId));
+
   if (queryOption === 'ALL') {
-    const postSnapshot = await getDocs(q);
+    const postSnapshot = await getDocs(queryWithUid);
 
     getData(postSnapshot);
   } else if (queryOption === 'ORDER_BY') {
-    const postSnapshot = await getDocs(query(q, orderBy(target, option)));
+    const postSnapshot = await getDocs(query(queryWithUid, orderBy(target, option)));
 
     console.log('orderby', postSnapshot);
 
     getData(postSnapshot);
   } else {
-    const postSnapshot = await getDocs(query(q, where(queryOption, '==', target)));
+    const postSnapshot = await getDocs(query(queryWithUid, where(queryOption, '==', target)));
 
     getData(postSnapshot);
   }
