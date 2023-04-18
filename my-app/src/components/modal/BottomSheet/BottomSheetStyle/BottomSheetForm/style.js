4d5ca5cd34564b6bb8f84ddc4397d3b4315f363a
wSeungMi <seungmi.dev@gmail.com>
2023-04-18 14:29:30 +0900

    Design: Loding 아이콘 반영 및 UX 개선 #98

--- my-app/src/components/modal/BottomSheet/BottomSheetStyle/BottomSheetForm/style.js
+++ my-app/src/components/modal/BottomSheet/BottomSheetStyle/BottomSheetForm/style.js
@@ -1,6 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import Theme from '../../../../../styles/Theme';
 import MyLocationIcon from '../../../../../assets/Icon-MyLocation.png';
+import MyLocationLoding from '../../../../../assets/Icon-MyLocation-loding.png';
+
+const spin = keyframes`
+  from {
+    transform: rotate(0);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 export const Header = styled.div`
   position: relative;
@@ -62,7 +73,7 @@ export const LocationBtnBox = styled.div`
 `;
 
 export const MyLocation = styled.button`
-  display: flex;
+  position: relative;
 
   &:hover,
   &:active {
@@ -73,12 +84,18 @@ export const MyLocation = styled.button`
     content: '';
     display: inline-block;
     position: absolute;
-    left: 2rem;
-    background-image: url(${MyLocationIcon});
+    margin-top: -0.18rem;
+    left: -2rem;
+    background-image: url(${({ isLocationCheck }) =>
+      isLocationCheck ? MyLocationLoding : MyLocationIcon});
     background-size: cover;
-    width: 1.4rem;
-    height: 1.4rem;
-  }
+    width: 1.8rem;
+    height: 1.8rem;
+    animation: ${({ isLocationCheck }) =>
+      isLocationCheck &&
+      css`
+        ${spin} 2000ms linear infinite;
+      `}
 `;
 
 export const SectionBorder = styled.div`
