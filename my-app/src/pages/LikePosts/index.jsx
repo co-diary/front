df3797caa3968053d909549cce8069e50adecc7d
hb_kim <binikim66@gmail.com>
2023-04-20 00:35:56 +0900

    Feat: liked 컬렉션에서 해당 유저의 게시글 최신순으로 불러오기 #119

--- my-app/src/pages/LikePosts/index.jsx
+++ my-app/src/pages/LikePosts/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
 import Header from '../../components/common/Header';
 import NavBar from '../../components/common/NavBar';
 import * as S from './style';
+import { authState } from '../../atom/authRecoil';
+import { db } from '../../firebase';
 
 function LikePosts() {
+  const user = useRecoilValue(authState);
+  const likedRef = collection(db, 'liked');
+  const [likedPostList, setLikedPostList] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      addLikedListener();
+    }
+  }, [user]);
+
+  const addLikedListener = () => {
+    const q = query(likedRef, where('uid', '==', user.uid));
+
+    onSnapshot(q, (querySnapshot) => {
+      const listArr = [];
+
+      querySnapshot.forEach((doc) => {
+        listArr.push({ ...doc.data(), key: doc.id });
+      });
+      listArr.sort((a, b) => b.createAt.seconds - a.createAt.seconds);
+
+      setLikedPostList(listArr);
+    });
+  };
+
+  console.log('likedPostList', likedPostList);
+
   return (
     <>
       <Header title='좋아요' />
