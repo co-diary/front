28c9bc16d39fd7130f2db89b4512a799d654fdd5
wSeungMi <seungmi.dev@gmail.com>
2023-04-19 17:19:55 +0900

    Feat: 검색결과창에 입력된 키워드값 props로 전달 #98

--- my-app/src/components/post/LocationSearch/index.jsx
+++ my-app/src/components/post/LocationSearch/index.jsx
@@ -4,8 +4,10 @@ import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import * as S from './style';
 import placeState from '../../../atom/mapRecoil';
 
-function LocationSearch() {
+function LocationSearch({ searchKeyword }) {
   const defaultLocation = useRecoilValue(placeState);
+
+  console.log('결과 props:', searchKeyword);
 
   return (
     <>
@@ -20,46 +22,6 @@ function LocationSearch() {
         <S.SectionTitle>위치</S.SectionTitle>
         <S.ResultBox>
           <S.Results>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
-            <S.Result>
-              <S.ResultTitle>이디야</S.ResultTitle>
-              <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
-            </S.Result>
             <S.Result>
               <S.ResultTitle>이디야</S.ResultTitle>
               <S.ResultDetail>대한민국 서울특별시 청파로71길 10 04304</S.ResultDetail>
