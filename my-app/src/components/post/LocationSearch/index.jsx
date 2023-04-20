6bd4608ec46b28a19fdb25a275ce1a982097a21d
wSeungMi <seungmi.dev@gmail.com>
2023-04-20 22:14:17 +0900

    Feat: 카카오맵 API에 키워드 검색 요청 기능 구현 및 마커 추가 #98

--- my-app/src/components/post/LocationSearch/index.jsx
+++ my-app/src/components/post/LocationSearch/index.jsx
@@ -1,21 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import * as S from './style';
 import placeState from '../../../atom/mapRecoil';
 
-function LocationSearch({ searchKeyword }) {
-  const defaultLocation = useRecoilValue(placeState);
+function LocationSearch({ searchKeyword, isInputNull }) {
+  const { kakao } = window;
+  const place = useRecoilValue(placeState);
 
-  console.log('결과 props:', searchKeyword);
+  const [info, setInfo] = useState();
+  const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState();
+
+  console.log('불러오기: ', markers, '마커클릭:', info);
+
+  useEffect(() => {
+    if (!map) return;
+
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(searchKeyword, (data, status, _pagination) => {
+      if (status === kakao.maps.services.Status.OK) {
+        const bounds = new kakao.maps.LatLngBounds();
+        const marker = [];
+
+        for (let i = 0; i < data.length; i++) {
+          // @ts-ignore
+          marker.push({
+            position: {
+              lat: data[i].y,
+              lng: data[i].x,
+            },
+            content: data[i].place_name,
+          });
+          // @ts-ignore
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+        setMarkers(marker);
+
+        map.setBounds(bounds);
+      } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+        console.log('검색 결과가 존재하지 않습니다.');
+      } else if (status === kakao.maps.services.Status.ERROR) {
+        console.log('검색 결과 중 오류가 발생했습니다.');
+      }
+    });
+  }, [searchKeyword]);
 
   return (
     <>
-      <div id='map' className='ir'>
-        <Map center={defaultLocation} style={{ width: '100%', height: '360px' }}>
-          <MapMarker position={defaultLocation}>
+      <div id='map'>
+        <Map center={place} style={{ width: '100%', height: '360px' }} level={3} onCreate={setMap}>
+          <MapMarker position={place}>
             <div style={{ color: '#000', align: 'center' }}>My co-diary!</div>
           </MapMarker>
+
+          {isInputNull &&
+            markers.map((marker) => (
+              <MapMarker
+                key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+                position={marker.position}
+                onClick={() => setInfo(marker)}
+              >
+                {info && info.content === marker.content && (
+                  <div style={{ color: '#000' }}>{marker.content}</div>
+                )}
+              </MapMarker>
+            ))}
         </Map>
       </div>
       <S.ResultSection>
