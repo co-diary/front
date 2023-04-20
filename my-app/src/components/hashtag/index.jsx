c7c767cf256e1bfedfd5d3999d87559d2711f226
Kimseolha <kynwww@naver.com>
2023-04-20 18:07:20 +0900

    Refactor: 해시태그 검색 페이지 리액트쿼리로 마이그레이션 #108

--- my-app/src/components/hashtag/index.jsx
+++ my-app/src/components/hashtag/index.jsx
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 import { useNavigate } from 'react-router';
 import Header from '../common/Header';
 import NavBar from '../common/NavBar';
 import Container from './style';
 
-import getPost from '../../hooks/getPost';
 import NoHashTag from './NoHashtag';
 import HashtagList from './HashtagList';
+import { UserIdState } from '../../atom/authRecoil';
+import usePost from '../../hooks/usePost';
 
 function HashtagView() {
-  const [tagArr, setTagArr] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const userId = useRecoilValue(UserIdState);
   const navigate = useNavigate();
 
-  console.log(setTagArr);
-  console.log(tagArr);
+  const { isLoading, isError, data: posts } = usePost(userId, 'ALL');
 
-  useEffect(() => {
-    getPost('ALL').then((data) => {
-      const onlyTags = data.reduce((acc, cur) => [...acc, ...cur.tag], []);
+  if (isLoading) {
+    return <div>🌀 Loading 🌀 </div>;
+  }
 
-      setTagArr(onlyTags);
-      setIsLoading(false);
-    });
-  }, []);
+  if (isError) {
+    return <div>fetch data중 에러</div>;
+  }
 
-  console.log(isLoading);
+  const onlyTags = posts.reduce((acc, cur) => [...acc, ...cur.tag], []);
 
   const goResultPage = (contents) => {
     navigate('/hashtag/keyword', { state: { data: contents } });
@@ -35,8 +34,8 @@ function HashtagView() {
     <>
       <Header title='태그 모아보기' />
       <Container>
-        {tagArr.length > 0 && <HashtagList tagArr={tagArr} goResultPage={goResultPage} />}
-        {!isLoading && tagArr.length === 0 && <NoHashTag />}
+        {onlyTags.length > 0 && <HashtagList tagArr={onlyTags} goResultPage={goResultPage} />}
+        {!isLoading && onlyTags.length === 0 && <NoHashTag />}
       </Container>
       <NavBar />
     </>
