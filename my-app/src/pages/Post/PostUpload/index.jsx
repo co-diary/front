8c0db5c79a36956db68a468c2b60d5fffe7d4adf
wSeungMi <seungmi.dev@gmail.com>
2023-05-12 10:25:54 +0900

    Feat: 등록버튼에 confirm modal 연결 #98

--- my-app/src/pages/Post/PostUpload/index.jsx
+++ my-app/src/pages/Post/PostUpload/index.jsx
@@ -4,10 +4,15 @@ import Header from '../../../components/common/Header';
 import NavBar from '../../../components/common/NavBar';
 import Button from '../../../components/common/Button';
 import PostForm from '../../../components/post/PostForm';
+import useToggle from '../../../hooks/useToggle';
+import Portal from '../../../components/modal/Portal';
+import ConfirmModal from '../../../components/modal/ConfirmModal';
 import { inputValidState } from '../../../atom/postUploadRecoil';
 
 function PostUpload() {
   const inputValid = useRecoilValue(inputValidState);
+
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useToggle();
 
   const btnDisabled =
     !inputValid.addressValid ||
@@ -17,13 +22,38 @@ function PostUpload() {
     !inputValid.ratingValid ||
     !inputValid.storeValid;
 
+  const handlePostUploadConfirm = () => {
+    setIsConfirmModalOpen();
+  };
+
+  const confirmModalClose = () => {
+    setIsConfirmModalOpen();
+  };
+
   return (
     <>
       <Header
         title='오늘 작성할 커디어리'
-        rightChild={<Button size='sm' text='등록' btnDisabled={btnDisabled} />}
+        rightChild={
+          <Button
+            size='sm'
+            text='등록'
+            btnDisabled={btnDisabled}
+            onClick={handlePostUploadConfirm}
+          />
+        }
       />
       <PostForm />
+      <Portal>
+        <ConfirmModal
+          visible={isConfirmModalOpen}
+          msg='게시글을 등록할까요?'
+          leftBtnMsg='취소'
+          rightBtnMsg='등록'
+          onClickClose={confirmModalClose}
+          leftOnclick={confirmModalClose}
+        />
+      </Portal>
       <NavBar page='upload' />
     </>
   );
