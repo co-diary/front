4d0d206c1c26d748f26b4fa07c204e617d94ba2d
Kimseolha <kynwww@naver.com>
2023-07-17 17:46:42 +0900

    Refactor: PostCard 컴포넌트 내의 UpdatePost 함수 추상화 #173

---
+++ src/hooks/usePostLikedUpdate.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { deleteDoc, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+
+const usePostLikedUpdate = (postId, newLiked) => {
+  useEffect(() => {
+    const updatePostLiked = async () => {
+      const postDoc = doc(db, 'post', postId);
+      const newField = { like: newLiked };
+
+      await updateDoc(postDoc, newField);
+
+      if (newLiked) {
+        const postData = (await getDoc(postDoc)).data();
+
+        await setDoc(doc(db, 'liked', postId), {
+          ...postData,
+          like: newLiked,
+        });
+      } else {
+        await deleteDoc(doc(db, 'liked', postId));
+      }
+    };
+
+    updatePostLiked();
+  }, [postId, newLiked]);
+};
+
+export default usePostLikedUpdate;
