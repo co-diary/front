04892cf90e7b9c8a03b6e1947f214a62a9310d21
Kimseolha <kynwww@naver.com>
2023-08-17 22:37:02 +0900

    Feat: 클러스터 클릭 시 레벨 1씩 확대하는 기능 추가 #178

--- src/components/location/LazyMap.jsx
+++ src/components/location/LazyMap.jsx
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Map, MapMarker, MarkerClusterer, ZoomControl } from 'react-kakao-maps-sdk';
 import MyLocationMarker from './MyLocationMarker';
+import OverlayInfo from './OverlayInfo';
+import OptionButton from './OptionButton';
 
 function LazyMap({ myLocation, locationState, userPost, likedPost, onZoomChanged }) {
   console.log(likedPost);
   const CLUSTER_LEVEL = 9;
+
+  const [selectedMarkerPosition, setSelectedMarkerPosition] = useState(null);
+
+  const handleMarkerClick = (marker) => {
+    setSelectedMarkerPosition({ lat: marker[0], lng: marker[1] });
+    console.log(selectedMarkerPosition);
+    // 원하는 동작 수행
+  };
+
+  const mapRef = useRef();
+
+  const onClusterclick = (_target, cluster) => {
+    const map = mapRef.current;
+    // 현재 지도 레벨에서 1레벨 확대한 레벨
+    const level = map.getLevel() - 1;
+
+    // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
+    map.setLevel(level, { anchor: cluster.getCenter() });
+  };
 
   return (
     <Map
@@ -13,14 +34,25 @@ function LazyMap({ myLocation, locationState, userPost, likedPost, onZoomChanged
           ? { lat: locationState[0], lng: locationState[1] }
           : { lat: myLocation.latitude, lng: myLocation.longitude }
       }
-      style={{ width: '100%', height: `calc(100vh - 6rem)`, overflow: 'hidden' }}
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: `calc(100vh - 6rem)`,
+        overflow: 'hidden',
+      }}
       level={3}
+      ref={mapRef}
       onZoomChanged={(map) => onZoomChanged(map.getLevel())}
     >
       <MyLocationMarker myLocation={myLocation} />
 
       {userPost && (
-        <MarkerClusterer averageCenter={true} minLevel={CLUSTER_LEVEL}>
+        <MarkerClusterer
+          averageCenter={true}
+          minLevel={CLUSTER_LEVEL}
+          disableClickZoom={true}
+          onClusterclick={onClusterclick}
+        >
           {userPost.map((post) => (
             <MapMarker
               key={`${post[0]}-${post[1]}`}
@@ -30,7 +62,9 @@ function LazyMap({ myLocation, locationState, userPost, likedPost, onZoomChanged
               }}
               clickable={true}
               onClick={() => handleMarkerClick(post)}
-            ></MapMarker>
+            >
+              {selectedMarkerPosition && <OverlayInfo markerPosition={selectedMarkerPosition} />}
+            </MapMarker>
           ))}
         </MarkerClusterer>
       )}
@@ -47,9 +81,7 @@ function LazyMap({ myLocation, locationState, userPost, likedPost, onZoomChanged
             onClick={() => handleMarkerClick()}
           />
         ))}
-      <button>버튼1</button>
-      <button>버튼2</button>
-      <button>버튼3</button>
+      <OptionButton></OptionButton>
     </Map>
   );
 }
