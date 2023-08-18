8553072ff3ddc8569ff4f3aa21a73de19350c461
Kimseolha <kynwww@naver.com>
2023-08-18 15:51:26 +0900

    Refactor: useGetLocation hook 수정 - getLocation, myLocation 둘 다 반환하도록 #178

--- src/hooks/useGetLocation.js
+++ src/hooks/useGetLocation.js
@@ -1,13 +1,9 @@
 import { useState, useEffect } from 'react';
 
-// 현재 위치 정보를 처리하는 컴포넌트 (데이터 다루는 컴포넌트)
 function useGetLocation() {
   const [myLocation, setMyLocation] = useState(null);
 
-  console.log(navigator.geolocation.getCurrentPostiion);
-
   const successHandler = (response) => {
-    console.log(response); // coords: GeolocationCoordinates {latitude: 위도, longitude: 경도, …} timestamp: 1673446873903
     const { latitude, longitude } = response.coords;
 
     setMyLocation({ latitude, longitude });
@@ -17,13 +13,23 @@ function useGetLocation() {
     console.log(error);
   };
 
+  const getLocation = async () => {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+
+      successHandler(response);
+    } catch (error) {
+      errorHandler(error);
+    }
+  };
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
+    getLocation();
   }, []);
 
-  console.log(`현재 위치는 ${myLocation}입니다`);
-
-  return myLocation;
+  return { myLocation, getLocation };
 }
 
 export default useGetLocation;
