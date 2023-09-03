06fae82b839986d9b4427cd389da7cb33812189a
Kimseolha <kynwww@naver.com>
2023-09-03 22:20:44 +0900

    Feat: NoRecentPosts 컴포넌트 추가 #183

---
+++ src/components/home/NoRecentPosts/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style';
+
+function NoRecentPosts() {
+  return (
+    <S.TextContainer>
+      <S.Text>최근 작성한 기록이 없어요.</S.Text>
+    </S.TextContainer>
+  );
+}
+
+export default NoRecentPosts;
