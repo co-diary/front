edf79b141a45936df7212be42d16e112b58b6873
Kimseolha <kynwww@naver.com>
2023-09-14 10:24:54 +0900

    Feat: 맵 오버레이 이미지 UI 수정 (이미지 크게) #191

--- src/components/location/OverlayInfo/style.js
+++ src/components/location/OverlayInfo/style.js
@@ -3,17 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  padding: 10px;
   background-color: white;
   border-radius: 5px;
   box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
 `;
 
 export const Image = styled.img`
-  width: 6rem;
-  height: 6rem;
+  width: 7rem;
+  height: 100%;
   object-fit: cover;
-  border-radius: 50%;
   margin-right: 10px;
 `;
 
@@ -21,6 +20,7 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  padding: 10px;
 
   strong {
     font-family: LINESeedKR-Bd;
